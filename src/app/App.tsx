e4ca8c7d8370c502bfdfba1f01842a22aee17313
Daniel Tognon <37959139+xDellTog@users.noreply.github.com>
2022-03-04 11:17:54 -0300

    fix | title and emoji hand component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
+import EmojiHand from "./components/emoji-hand/EmojiHand";
 
 function App() {
     return (
         <div className="d-flex align-items-center justify-content-center vh-100 vw-100">
-            <h1>Hi there, I'm Daniel! 👋🏼</h1>
+            <h1> Hi there, I'm Daniel! </h1>
+            <EmojiHand/>
         </div>
     );
 }
---
+++ src/app/components/emoji-hand/EmojiHand.tsx
@@ -0,0 +1,7 @@
+import './EmojiHand.scss';
+
+export default function EmojiHand() {
+    return (
+        <h1 className="shake-animation mx-2">👋🏼</h1>
+    )
+}

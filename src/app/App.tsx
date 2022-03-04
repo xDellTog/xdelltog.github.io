f2b1f64bb70c57f5f40a7401f1a649c1af386171
Daniel Tognon <37959139+xDellTog@users.noreply.github.com>
2022-03-04 10:47:41 -0300

    feat | new home

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import logo from './assets/logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link"
-                   href="https://reactjs.org"
-                   target="_blank"
-                   rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+        <div className="d-flex align-items-center justify-content-center vh-100 vw-100">
+            <h1>Hi there, I'm Daniel! 👋🏼</h1>
         </div>
     );
 }

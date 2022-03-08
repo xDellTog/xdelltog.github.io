70e8b92324ff1a7ce5db13e610e20ad7457dab59
Daniel Tognon <37959139+xDellTog@users.noreply.github.com>
2022-03-08 15:24:30 -0300

    feat | installed stitches and radix-colors

--- src/app/pages/home-page/HomePage.tsx
+++ src/app/pages/home-page/HomePage.tsx
@@ -10,11 +10,58 @@ export default function HomePage() {
     }
 
     return (
-        <div className="d-flex flex-column align-items-center justify-content-center vh-100 vw-100">
-            <div className="d-flex align-items-center justify-content-center">
-                <h1 onClick={goToAbout}> Hi there, I'm Daniel! </h1>
-                <EmojiHand/>
-            </div>
+        <div>
+            <nav className="position-fixed w-100 top-0 d-flex p-3 align-items-center justify-content-between bg-slate">
+                <a href="#home" className="btn fw-light">
+                    <h1 className="m-0">💻</h1>
+                </a>
+                <div className="d-flex align-items-center justify-content-end">
+                    <a href="#about" className="btn fw-light">
+                        About
+                    </a>
+                    <a href="#projects" className="btn fw-light">
+                        Projects
+                    </a>
+                    <a href="#contact" className="btn fw-light">
+                        Contact
+                    </a>
+                </div>
+            </nav>
+
+            <section className="d-flex flex-column align-items-center justify-content-center vh-100 vw-100"
+                     id="home">
+                <div className="d-flex align-items-center justify-content-center">
+                    <h1 onClick={goToAbout}> Hi there, I'm Daniel! </h1>
+                    <EmojiHand/>
+                </div>
+                <h5 className="fw-light">Web and Mobile developer</h5>
+            </section>
+
+            <section className="d-flex flex-column align-items-center justify-content-center vh-100 vw-100"
+                     id="about">
+                <h1>👨🏻‍💻</h1>
+                <div className="row col-12 col-md-8">
+                    <h3 className="text-center fw-light">
+                        I'm front-end developer based in the Brazil. <br/>
+                        With more than 3 years working with full stack development, <br/>
+                        more than 10 programming languages
+                        and many tools and frameworks
+                    </h3>
+                </div>
+            </section>
+
+            <section className="d-flex flex-column align-items-center justify-content-center vh-100 vw-100"
+                     id="projects">
+                <h1>👨🏻‍💻</h1>
+                <div className="row col-12 col-md-8">
+                    <h3 className="text-center fw-light">
+                        I'm front-end developer based in the Brazil. <br/>
+                        With more than 3 years working with full stack development, <br/>
+                        more than 10 programming languages
+                        and many tools and frameworks
+                    </h3>
+                </div>
+            </section>
         </div>
     );
 }
